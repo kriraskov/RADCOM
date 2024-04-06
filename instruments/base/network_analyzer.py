2c98feb9140d5f30c5ae12f9efe45459ecb88e43
kriraskov <kristoffer.raskov@gmail.com>
2024-04-06 13:15:19 +0200

    Documentation.

--- instruments/base/network_analyzer.py
+++ instruments/base/network_analyzer.py
@@ -4,138 +4,124 @@ from typing import Literal
 import pyvisa
 
 
-SweepType = Literal['lin', 'log', 'power']
-
-FrequencyUnit = Literal['Hz', 'kHz', 'MHz', 'GHz']
-
-ParameterFormat = Literal['lin_phase', 'log_phase', 'real_imag']
-
-
 class NetworkAnalyzer(ABC, Instrument):
-    def __init__(self, resource_name: str, query_delay: float = 0.,
-                 timeout: int = 2000, write_termination: str = None,
-                 read_termination: str = None, echo: bool = False):
-        super().__init__(resource_name, query_delay, timeout,
-                         write_termination, read_termination, echo)
-        self.ports = list()
-
     @property
     @abstractmethod
-    def sweep_type(self):
+    def sweep_type(self) -> str:
         ...
 
     @sweep_type.setter
     @abstractmethod
-    def sweep_type(self, value: SweepType):
+    def sweep_type(self, value: str) -> None:
         ...
 
     @property
     @abstractmethod
-    def center_frequency(self):
+    def center_frequency(self) -> float:
         ...
 
     @center_frequency.setter
     @abstractmethod
-    def center_frequency(self, value: float):
+    def center_frequency(self, value: float) -> None:
         ...
 
     @property
     @abstractmethod
-    def frequency_span(self):
+    def frequency_span(self) -> float:
         ...
 
     @frequency_span.setter
     @abstractmethod
-    def frequency_span(self, value: float):
+    def frequency_span(self, value: float) -> None:
         ...
 
     @property
     @abstractmethod
-    def start_frequency(self):
+    def start_frequency(self) -> float:
         ...
 
     @start_frequency.setter
     @abstractmethod
-    def start_frequency(self, value: float):
+    def start_frequency(self, value: float) -> None:
         ...
 
     @property
     @abstractmethod
-    def stop_frequency(self):
+    def stop_frequency(self) -> float:
         ...
 
     @stop_frequency.setter
     @abstractmethod
-    def stop_frequency(self, value: float):
+    def stop_frequency(self, value: float) -> None:
         ...
 
     @property
     @abstractmethod
-    def cw_frequency(self):
+    def cw_frequency(self) -> float:
         ...
 
     @cw_frequency.setter
     @abstractmethod
-    def cw_frequency(self, value: float):
+    def cw_frequency(self, value: float) -> None:
         ...
 
     @property
     @abstractmethod
-    def frequency_sweep_length(self):
+    def frequency_sweep_length(self) -> int:
         ...
 
     @frequency_sweep_length.setter
     @abstractmethod
-    def frequency_sweep_length(self, value: int):
+    def frequency_sweep_length(self, value: int) -> None:
         ...
 
     @property
     @abstractmethod
-    def snp_frequency_units(self):
+    def snp_frequency_units(self) -> str:
         ...
 
     @snp_frequency_units.setter
     @abstractmethod
-    def snp_frequency_units(self, value: FrequencyUnit):
+    def snp_frequency_units(self, value: str) -> None:
         ...
 
     @property
     @abstractmethod
-    def snp_parameter_format(self):
+    def snp_parameter_format(self) -> str:
         ...
 
     @snp_parameter_format.setter
     @abstractmethod
-    def snp_parameter_format(self, value: ParameterFormat):
+    def snp_parameter_format(self, value: str) -> None:
         ...
 
     @abstractmethod
-    def get_marker(self, number: int = 1, trace: int = 1):
+    def get_marker(self, number: int = 1, trace: int = 1) -> float:
         ...
 
     @abstractmethod
-    def set_marker(self, x: float, number: int = 1, trace: int = 1):
+    def set_marker(self, x: float, number: int = 1, trace: int = 1) -> None:
         ...
 
     @abstractmethod
-    def read_marker(self, number: int = 1, trace: int = 1):
+    def read_marker(self, number: int = 1, trace: int = 1) -> float:
         ...
 
     @abstractmethod
-    def remote(self):
+    def remote(self) -> None:
         ...
 
     @abstractmethod
-    def return_to_local(self):
+    def return_to_local(self) -> None:
         ...
 
-    def close(self):
+    def close(self) -> None:
         self.return_to_local()
         super().close()
 
-    def config_freq_sweep(self, sweep_length: int, start: float = None,
+    def setup_freq_sweep(self, sweep_length: int, start: float = None,
                           stop: float = None, center: float = None,
-                          span: float = None, sweep_type: SweepType = 'lin'):
+                          span: float = None, sweep_type: str = None) -> None:
         self.sweep_type = sweep_type
         self.frequency_sweep_length = sweep_length
         if (start and stop) and not (center or span):
@@ -149,56 +135,42 @@ class NetworkAnalyzer(ABC, Instrument):
 
 
 class Port(ABC, Channel):
-    def __init__(self, number: int, resource: pyvisa.Resource,
-                 echo: bool = False, sync: bool = False):
-        super().__init__(number, resource, echo, sync)
-
     @property
     @abstractmethod
-    def power(self):
+    def power(self) -> float:
         ...
 
     @power.setter
     @abstractmethod
-    def power(self, value: int):
+    def power(self, value: float) -> None:
         ...
 
     @property
     @abstractmethod
-    def power_sweep_start(self):
+    def power_sweep_start(self) -> float:
         ...
 
     @power_sweep_start.setter
     @abstractmethod
-    def power_sweep_start(self, value: float):
+    def power_sweep_start(self, value: float) -> None:
         ...
 
     @property
     @abstractmethod
-    def power_sweep_stop(self):
+    def power_sweep_stop(self) -> float:
         ...
 
     @power_sweep_stop.setter
     @abstractmethod
-    def power_sweep_stop(self, value: float):
+    def power_sweep_stop(self, value: float) -> None:
         ...
 
     @property
     @abstractmethod
-    def power_sweep_length(self,):
+    def power_sweep_length(self) -> int:
         ...
 
     @power_sweep_length.setter
     @abstractmethod
-    def power_sweep_length(self, value: float):
+    def power_sweep_length(self, value: int) -> None:
         ...
-
-    def config_power_sweep(self, sweep_length: int, cw_frequency: float,
-                           start: float, stop: float):
-        super().sweep_type = 'power'
-        super().cw_frequency = cw_frequency
-        self.power_sweep_start = start
-        self.power_sweep_stop = stop
-        self.power_sweep_length = sweep_length
-
-
--- instruments/ms464xb.py
+++ instruments/ms464xb.py
@@ -1,5 +1,13 @@
 import pyvisa
 from .base import network_analyzer as vna
+from typing import Literal
+
+
+SweepType = Literal['lin', 'log', 'power']
+
+FrequencyUnit = Literal['Hz', 'kHz', 'MHz', 'GHz']
+
+ParameterFormat = Literal['lin_phase', 'log_phase', 'real_imag']
 
 
 class MS464xB(vna.NetworkAnalyzer):
@@ -9,7 +17,7 @@ class MS464xB(vna.NetworkAnalyzer):
     def __init__(self, resource_name: str, channel: int = 1,
                  query_delay: float = 0., timeout: int = 0,
                  write_termination: str = '\n', read_termination: str = '\r\n',
-                 echo: bool = False):
+                 echo: bool = False) -> None:
         super().__init__(resource_name, query_delay, timeout,
                          write_termination, read_termination, echo)
         self.channel = channel
@@ -18,106 +26,107 @@ class MS464xB(vna.NetworkAnalyzer):
         self.write('LANG NATIVE')
 
     @property
-    def sweep_type(self):
+    def sweep_type(self) -> str:
         return self.query(f':SENS{self.channel}:SWE:TYP?')
 
     @sweep_type.setter
-    def sweep_type(self, value: vna.SweepType):
+    def sweep_type(self, value: SweepType) -> None:
         self.write(f':SENS{self.channel}:SWE:TYP ' + value)
 
     @property
-    def center_frequency(self):
+    def center_frequency(self) -> float:
         return float(self.query(f':SENS{self.channel}:FREQ:CENT?'))
 
     @center_frequency.setter
-    def center_frequency(self, value: float):
+    def center_frequency(self, value: float) -> None:
         self.write(f':SENS{self.channel}:FREQ:CENT {value}')
 
     @property
-    def frequency_span(self):
+    def frequency_span(self) -> float:
         return float(self.query(f':SENS{self.channel}:FREQ:SPAN?'))
 
     @frequency_span.setter
-    def frequency_span(self, value: float):
+    def frequency_span(self, value: float) -> None:
         self.write(f':SENS{self.channel}:FREQ:SPAN {value}')
 
     @property
-    def start_frequency(self):
+    def start_frequency(self) -> float:
         return float(self.query(f':SENS{self.channel}:FREQ:STAR?'))
 
     @start_frequency.setter
-    def start_frequency(self, value: float):
+    def start_frequency(self, value: float) -> None:
         self.write(f':SENS{self.channel}:FREQ:START {value}')
 
     @property
-    def stop_frequency(self):
+    def stop_frequency(self) -> float:
         return float(self.query(f':SENS{self.channel}:FREQ:STOP?'))
 
     @stop_frequency.setter
-    def stop_frequency(self, value: float):
+    def stop_frequency(self, value: float) -> None:
         self.write(f':SENS{self.channel}:FREQ:STOP {value}')
 
     @property
-    def cw_frequency(self):
+    def cw_frequency(self) -> float:
         return float(self.query(f':SENS{self.channel}:FREQ:CW?'))
 
     @cw_frequency.setter
-    def cw_frequency(self, value: float):
+    def cw_frequency(self, value: float) -> None:
         self.write(f':SENS{self.channel}:FREQ:CW {value}')
 
     @property
-    def frequency_sweep_length(self):
-        return float(self.query(f':SENS{self.channel}:SWE:POIN?'))
+    def frequency_sweep_length(self) -> int:
+        return int(self.query(f':SENS{self.channel}:SWE:POIN?'))
 
     @frequency_sweep_length.setter
-    def frequency_sweep_length(self, value: int):
+    def frequency_sweep_length(self, value: int) -> None:
         self.write(f':SENS{self.channel}:SWE:POIN {value}')
 
     @property
-    def snp_frequency_units(self):
+    def snp_frequency_units(self) -> str:
         return self.query(':FORM:SNP:FREQ?')
 
     @snp_frequency_units.setter
-    def snp_frequency_units(self, value: vna.FrequencyUnit):
+    def snp_frequency_units(self, value: FrequencyUnit) -> None:
         self.write(f':FORM:SNP:FREQ {value}')
 
     @property
-    def snp_parameter_format(self):
+    def snp_parameter_format(self) -> str:
         return self.query(':FORM:SNP:PAR?')
 
     @snp_parameter_format.setter
-    def snp_parameter_format(self, value: vna.ParameterFormat):
+    def snp_parameter_format(self, value: ParameterFormat) -> None:
         self.write(f':FORM:SNP:PAR {self.PARAM_FORMATS[value]}')
 
     @property
-    def marker_format(self):
+    def marker_format(self) -> str:
         return self.query(f':CALC{self.channel}:FORM?')
 
     @marker_format.setter
-    def marker_format(self, fmt: vna.ParameterFormat):
+    def marker_format(self, fmt: ParameterFormat) -> None:
         self.write(f':CALC{self.channel}:FORM {fmt}')
 
-    def get_marker(self, number: int = 1, trace: int = 1):
+    def get_marker(self, number: int = 1, trace: int = 1) -> float:
         return float(self.query(
             f':CALC{self.channel}:PAR{trace}:MARK{number}:X?'))
 
-    def set_marker(self, x: float, number: int = 1, trace: int = 1):
+    def set_marker(self, x: float, number: int = 1, trace: int = 1) -> None:
         self.write(f':CALC{self.channel}:PAR{trace}:MARK{number}:ACT')
         self.write(f':CALC{self.channel}:PAR{trace}:MARK{number}:X {x}')
 
-    def read_marker(self, number: int = 1, trace: int = 1):
-        return self.query(f':CALC{self.channel}:PAR{trace}:MARK{number}:Y?')
+    def read_marker(self, number: int = 1, trace: int = 1) -> float:
+        return float(self.query(
+            f':CALC{self.channel}:PAR{trace}:MARK{number}:Y?'))
 
-    def remote(self):
+    def remote(self) -> None:
         # MS464xB switches to remote when any command is written.
         self.query('*IDN?')
 
-    def return_to_local(self):
+    def return_to_local(self) -> None:
         print('RTL')
         self.write('RTL')
 
     def measure(self, x: float, marker_number: int = 1, trace: int = 1,
-                fmt: vna.ParameterFormat = 'lin_phase'):
+                fmt: ParameterFormat = 'lin_phase') -> float:
         self.marker_format = fmt
         self.set_marker(x, marker_number, trace)
         self.write('TRS;WFS')
@@ -128,44 +137,45 @@ class MS464xB(vna.NetworkAnalyzer):
 
 
 class MS464xBPort(vna.Port):
-    def __init__(self, number: int, channel: int, resource: pyvisa.Resource):
+    def __init__(self, number: int, channel: int, resource: pyvisa.Resource)\
+            -> None:
         super().__init__(number, resource)
         self.channel = channel
 
     @property
-    def power(self):
-        return self.query(f':SOUR{self.channel}:POW:PORT{self.port_number}?')
+    def power(self) -> float:
+        return float(self.query(f':SOUR{self.channel}:POW:PORT{self.number}?'))
 
     @power.setter
-    def power(self, value: float):
-        self.write(f':SOUR{self.channel}:POW:PORT{self.port_number} {value}')
+    def power(self, value: float) -> None:
+        self.write(f':SOUR{self.channel}:POW:PORT{self.number} {value}')
 
     @property
-    def power_sweep_start(self):
-        return self.query(f':SOUR{self.channel}:POW:PORT{self.port_number}'
-                          f':LIN:POW:STAR?')
+    def power_sweep_start(self) -> float:
+        return float(self.query(
+            f':SOUR{self.channel}:POW:PORT{self.number}:LIN:POW:STAR?'))
 
     @power_sweep_start.setter
-    def power_sweep_start(self, value: float):
-        self.write(f':SOUR{self.channel}:POW:PORT{self.port_number}'
-                   f':LIN:POW:STAR {value}')
+    def power_sweep_start(self, value: float) -> None:
+        self.write(
+            f':SOUR{self.channel}:POW:PORT{self.number}:LIN:POW:STAR {value}')
 
     @property
-    def power_sweep_stop(self):
-        return self.query(
-            f':SOUR{self.channel}:POW:PORT{self.port_number}:LIN:POW:STOP?')
+    def power_sweep_stop(self) -> float:
+        return float(self.query(
+            f':SOUR{self.channel}:POW:PORT{self.number}:LIN:POW:STOP?'))
 
     @power_sweep_stop.setter
-    def power_sweep_stop(self, value: float):
-        self.write(f':SOUR{self.channel}:POW:PORT{self.port_number}'
-                   f':LIN:POW:STOP {value}')
+    def power_sweep_stop(self, value: float) -> None:
+        self.write(
+            f':SOUR{self.channel}:POW:PORT{self.number}:LIN:POW:STOP {value}')
 
     @property
-    def power_sweep_length(self):
-        return self.query(f':SOUR{self.channel}:POW:PORT{self.port_number}'
-                          f':LIN:POW:POIN?')
+    def power_sweep_length(self) -> int:
+        return int(self.query(
+            f':SOUR{self.channel}:POW:PORT{self.number}:LIN:POW:POIN?'))
 
     @power_sweep_length.setter
-    def power_sweep_length(self, value: float):
-        self.write(f':SOUR{self.channel}:POW:PORT{self.port_number}'
-                   f':LIN:POW:POIN {value}')
+    def power_sweep_length(self, value: int) -> None:
+        self.write(
+            f':SOUR{self.channel}:POW:PORT{self.number}:LIN:POW:POIN {value}')
