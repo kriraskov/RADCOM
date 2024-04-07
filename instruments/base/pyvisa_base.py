bca0c9cdcd199e567e8166333ab419d8126fc70c
kriraskov <kristoffer.raskov@gmail.com>
2024-04-07 09:17:16 +0200

    Documentation.

--- instruments/base/pyvisa_base.py
+++ instruments/base/pyvisa_base.py
@@ -7,8 +7,8 @@ class PyVisaBase:
     Wraps the PyVisa API to provide an abstraction of its interface.
     This class represents a PyVisa resource and the properties needed to
     communicate with it. If no resource is provided at instantiation,
-    open a new resource with :meth:`open_resource()`. List available
-    resources with :meth:`list_resources()`.
+    open a new resource with `open_resource()`. List available
+    resources with `list_resources()`.
 
     Attributes:
         resource (pyvisa.Resource): Reference to the PyVisa resource
@@ -59,7 +59,19 @@ class PyVisaBase:
     def open_resource(self, resource_name: str, query_delay: float,
                       timeout: int, read_termination: str,
                       write_termination: str) -> None:
-        """Open a resource by name and set its attributes."""
+        """Open a resource by name and set its attributes.
+
+        Args:
+            resource_name (str): Name of the resource to open.
+            query_delay (float): Delay in seconds between write and
+                read.
+            timeout (float): Time in milliseconds before interrupt of
+                unanswered reads.
+            read_termination (str): Termination character of read
+                strings.
+            write_termination (str): Termination character of written
+                commands.
+        """
         rm = pyvisa.ResourceManager()
         self.resource = rm.open_resource(resource_name)
         self.read_termination = read_termination
@@ -84,7 +96,7 @@ class PyVisaBase:
         return self.resource.query(cmd)
 
     def close(self) -> None:
-        """Close PyVisa resource."""
+        """Close the PyVisa resource."""
         self.resource.close()
 
     @staticmethod
